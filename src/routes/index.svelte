aa0e51097003532c42f40d8ac9bd7d7769de465d
bigmooninc <aaron@bigmooninc.com>
2021-05-13 13:57:57 -0500

    landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,9 +28,9 @@
 
 <div class="border-b border-gray-800">
   <div class="w-full max-w-5xl mx-auto px-5 md:px-0">
-    <div class="flex flex-col justify-center items-start pt-48 pb-32">
-      <h1>Rideshare Analytics</h1>
-      <p>
+    <div class="flex flex-col justify-center items-start pt-48 pb-32 w-2/3">
+      <h1>How much is your drive time worth?</h1>
+      <p class="mr-32">
         Get accurate analytics to help inform when your time driving is the most
         profitable.
       </p>
