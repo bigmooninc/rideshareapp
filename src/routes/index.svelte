826e914e28763076e3d1366bc53559e507df5980
bigmooninc <aaron@bigmooninc.com>
2021-05-13 18:57:56 -0500

    landing page styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,17 +29,17 @@
 <div class="border-b border-gray-800">
   <div class="w-full max-w-5xl mx-auto px-5 md:px-0">
     <div
-      class="flex flex-col justify-center items-start pt-48 pb-32 w-full md:w-1/2"
+      class="flex flex-col justify-center items-start pt-48 pb-32 w-full md:w-2/3"
     >
-      <h1>How much is your time worth?</h1>
-      <p class="mr-0 md:overflow-y-hiddenmr-32">
+      <h1>How much is your drive worth?</h1>
+      <p class="mr-0 md:mr-32 md:overflow-y-hiddenmr-32">
         Get accurate analytics to help inform when your time driving is the most
         profitable.
       </p>
       <div class="flex flex-row items-center">
         <a
           href="register"
-          class="register_link border rounded-full font-bold text-base px-10 py-3"
+          class="register_link border rounded-full font-bold text-base px-8 py-3"
           >Get Started</a
         >
         <a href="login" class="login_link text-white bg-none px-8">Sign In</a>
@@ -55,7 +55,7 @@
 <style>
   h1 {
     font-family: "Montserrat", sans-serif;
-    @apply font-bold text-white text-5xl mb-3;
+    @apply font-bold text-white text-6xl mb-3;
   }
   p {
     font-family: "Montserrat", sans-serif;
